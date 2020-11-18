13cd8cb0f47073a87250ed789f4cf843ee9f26fa
divya-sk <divya.kumar@citrusinformatics.com>
2020-11-18 09:51:55 +0530

    Update version changes.

--- core/deps.ts
+++ core/deps.ts
@@ -1,4 +1,4 @@
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.6/mod.ts";
 export * as namespaceMgr from "https://denopkg.com/shah/namespace-manager@v1.0.7/mod.ts";
-export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
-export * as valueMgr from "https://denopkg.com/shah/value-manager@v1.0.8/mod.ts";
+export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.5/mod.ts";
+export * as valueMgr from "https://denopkg.com/shah/value-manager@v1.0.9/mod.ts";
--- typical/deps.ts
+++ typical/deps.ts
@@ -1,4 +1,4 @@
-export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.0.24/mod.ts";
+export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.1.0/mod.ts";
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.6/mod.ts";
 export * as namespaceMgr from "https://denopkg.com/shah/namespace-manager@v1.0.7/mod.ts";
-export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
+export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.5/mod.ts";
