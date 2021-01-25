608379f8a69ffb6c0a34d6189efb165f614d2e27
Shahid N. Shah <shahid@shah.org>
2021-01-25 09:41:10 -0500

    fix: update dependencies

--- transform/deps.ts
+++ transform/deps.ts
@@ -1,3 +1,3 @@
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.7/mod.ts";
 export * as specModule from "https://denopkg.com/shah/specification-module@v1.0.9/mod.ts";
-export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.1.1/mod.ts";
+export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.2.0/mod.ts";
--- typical/deps.ts
+++ typical/deps.ts
@@ -1,4 +1,4 @@
-export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.1.1/mod.ts";
+export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.2.0/mod.ts";
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.7/mod.ts";
 export * as namespaceMgr from "https://denopkg.com/shah/namespace-manager@v1.0.8/mod.ts";
 export * as inflect from "https://denopkg.com/shah/text-inflect@v1.0.5/mod.ts";
